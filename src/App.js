bdf630ffde98e730eb8df62dd941f763eea40a25
javadjafariv94 <javadjafariv94@gmail.com>
2019-03-12 19:16:41 +0330

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      count:5
+    }
+  }
+
+  increment(){
+    console.log("increment");
+  }
+
+  decrement(){
+    console.log("decrement");
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div >
+         
+         <button onClick = {this.increment}> + </button>
+            <h2>count : {this.state.count}</h2>
+         <button onClick = {this.decrement}> - </button>
       </div>
     );
   }
